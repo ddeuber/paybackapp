95c469580751d0a7670bcd8e17c26f49ef5ada80
Anton Brucherseifer <wanton.amb@gmail.com>
2020-11-03 19:07:26 +0100

    feat: add some REST functions
    transaction list, add group and add transaction

--- app.py
+++ app.py
@@ -1,9 +1,72 @@
-from flask import Flask
-from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
-from database import app, db  
+#TODO:
+# add authentication (maybe this can also include the group password)
 
-api = Api(app) 
+import flask
+import flask_restful
+from flask_sqlalchemy import SQLAlchemy
+from database import app, db, Transaction, Involved, Group
+from datetime import datetime
+import json
+
+api = flask_restful.Api(app) 
+
+class AddGroup(flask_restful.Resource):
+    def post(self):
+        group_from_request = flask.request.get_json(force=True)
+        try:
+            group_for_database = Group(group_from_request)
+            db.session.add(group_for_database)
+            db.session.commit()
+            return {'id': group_for_database.id}, 201
+        except Exception as e:
+            if str(e).find('is not a string') != -1:
+                return str(e), 400
+            if str(e).find('UNIQUE constraint failed: group.name') != -1:
+                return 'group name \'' + group_from_request['name'] + '\' already exists', 400
+            return 'could not create group from your json', 400
+
+
+
+# add a single transaction
+class AddTransaction(flask_restful.Resource):
+    def post(self):
+        transaction_from_request = flask.request.get_json(force=True)
+        try:
+            transaction_for_database = Transaction(transaction_from_request)
+            if Group.query.filter_by(id=transaction_for_database.group_id).first() is None:
+                return 'group \'' + transaction_for_database.group_id + '\' does not exist', 400
+            db.session.add(transaction_for_database)
+            db.session.commit()
+        except Exception as e:
+            return 'could not create transaction from your json', 400
+        # Problem: at this point there is a transaction without involved
+        at_least_one_participant = False
+        for participant in transaction_from_request['involved']:
+            at_least_one_participant = True
+            involved = Involved(transaction_id=transaction_for_database.id, participant=participant)
+            db.session.add(involved)
+        if not at_least_one_participant:
+            db.session.delete(transaction_for_database)
+            db.session.commit()
+            return 'there must be at least one participant involved', 400
+        db.session.commit()
+        return {"added transaction": transaction_from_request}, 201
+
+class TransactionList(flask_restful.Resource):
+    def get(self, group_name):
+        transactions = []
+        try:
+            group_id = Group.query.filter_by(name=group_name).first().id
+        except:
+            return 'could not find group \'' + group_name + '\'', 400
+        for t in Transaction.query.filter_by(group_id=group_id):
+            transactions.append(t.to_dict())
+        return transactions
+
+
+api.add_resource(TransactionList, '/transactions/<string:group_name>')
+api.add_resource(AddGroup, '/add_group')
+api.add_resource(AddTransaction, '/add_transaction')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='::', debug=True)
--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Initialize database
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///payapp_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -15,6 +16,14 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, group_dict):
+        if not isinstance(group_dict['name'], str):
+            raise Exception('name is not a string')
+        if not isinstance(group_dict['password'], str):
+            raise Exception('password is not a string')
+        self.name = group_dict['name']
+        self.password = group_dict['password']
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,9 +36,35 @@ class Transaction(db.Model):
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
     group = db.relationship('Group', backref='transactions')
 
+    def __init__(self, transaction_dict):
+        self.creator=transaction_dict['creator']
+        self.payer=transaction_dict['payer']
+        self.amount=transaction_dict['amount']
+        self.comment=transaction_dict['comment']
+        self.creation_timestamp=transaction_dict['timestamp']
+        self.server_timestamp=datetime.now()
+        self.group_id = Group.query.filter_by(name=transaction_dict['group_name']).first().id
+
+    def to_dict(self):
+        participants = []
+        for involved_entry in Involved.query.filter_by(transaction_id=self.id):
+            participants.append(involved_entry.participant)
+        if len(participants) == 0:
+            raise Exception('no one is involved in transaction with id ' + str(self.id))
+        return {
+            #'id': self.id,
+            'creator': self.creator,
+            'payer': self.payer,
+            'amount': self.amount,
+            'comment': self.comment,
+            'timestamp': self.creation_timestamp,
+            'group_name': self.group.name,
+            'involved': participants
+        }
+
 class Involved(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    participant = db.Column(db.String(80), nullable=False)
     transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable=False, index=True)
     transaction = db.relationship('Transaction', backref="involved")
 
