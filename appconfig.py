ffe61d3b851ce42ccabc54c0bdb8d2096185f8a8
David Deuber <ddeuber@student.ethz.ch>
2020-12-29 14:17:20 +0100

    added comment for environment variables

--- appconfig.py
+++ appconfig.py
@@ -18,7 +18,7 @@ app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') 
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = bool(int(os.getenv('MAIL_USE_TLS')))
+app.config['MAIL_USE_TLS'] = bool(int(os.getenv('MAIL_USE_TLS'))) # booleans are encoded as 1 and 0
 app.config['MAIL_USE_SSL'] = bool(int(os.getenv('MAIL_USE_SSL')))
 app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
 
